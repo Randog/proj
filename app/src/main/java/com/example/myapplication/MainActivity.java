b56114cb31fc6b988cdaf2603603a5e40472d989
Kai Ko <kenko200176@gmail.com>
2020-09-12 16:24:08 -0400

    notification test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 //test
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dispNotif(View view) {
+
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "personal_notifications");
+            builder.setSmallIcon(R.drawable.ic_android_black_24dp);
+            builder.setContentTitle("adsfasd");
+            builder.setContentText("asdklfj;asd");
+            builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(001, builder.build());
+
     }
 }
